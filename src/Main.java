08f287841f2b1723a031e816c1783c846b08f49e
nagamrain <nagammrain@gmail.com>
2021-04-11 17:08:53 +0700

    Tugas 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,12 +18,35 @@ public class Main {
      *
      * @param args
      */
-    public static void main(String[] args) {
-        Kubus kubus = new Kubus(10);
-        Balok balok = new Balok(3,4,5);
+    public static void main(String[] args)  {
         
-        kubus.tampilkan();
-        balok.tampilkan();
+        Scanner input = new Scanner(System.in);
+        int sisi = 0, panjang = 0, lebar = 0, tinggi = 0;
+        
+        
+        try{
+            System.out.print (" Masukkan Sisi Kubus : ");
+            sisi = input.nextInt();
+            
+            System.out.print(" Masukkan Panjang Balok : ");
+            panjang = input.nextInt();
+            
+            System.out.print(" Masukkan Lebar Balok : ");
+            lebar = input.nextInt();
+            
+            System.out.print(" Masukkan Tinggi Balok : ");
+            tinggi = input.nextInt();
+        } catch(InputMismatchException error){
+            System.out.println(" Erornya adalah " + error.getMessage());
+        } finally {
+            Kubus kubus = new Kubus(sisi);
+            Balok balok = new Balok(panjang,lebar,tinggi);
+        
+            kubus.tampilkan();
+            balok.tampilkan();    
+        }
+        
+        
     }
   
 }
